9b702687de1edee06672a7dbe4fbe6eed2d5d179
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-05-02 16:52:06 +0200

    Update workflow

--- 02/github.tf
+++ 02/github.tf
@@ -19,3 +19,9 @@ resource "github_actions_variable" "boundary_addr" {
   variable_name = "BOUNDARY_ADDR"
   value         = var.hcp_boundary_cluster_url
 }
+
+resource "github_issue_label" "boundary" {
+  color      = "EC585D"
+  name       = "boundary"
+  repository = data.github_repository.this.name
+}
