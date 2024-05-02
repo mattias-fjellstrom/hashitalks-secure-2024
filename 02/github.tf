6dcc57dcfd9f340baf768a3dac436ab0dd28daa3
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-05-02 16:22:51 +0200

    Update workflow

--- 02/github.tf
+++ 02/github.tf
@@ -13,3 +13,9 @@ resource "github_actions_secret" "boundary_password" {
   secret_name     = "BOUNDARY_PASSWORD"
   plaintext_value = boundary_account_password.lambda.password
 }
+
+resource "github_actions_variable" "boundary_addr" {
+  repository    = data.github_repository.this.name
+  variable_name = "BOUNDARY_ADDR"
+  value         = var.hcp_boundary_cluster_url
+}
