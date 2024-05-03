e281c994de615a0dd63129e428b8954bbcd33a57
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-05-03 07:19:41 +0200

    Update workflow

--- 02/boundary.users.tf
+++ 02/boundary.users.tf
@@ -22,3 +22,12 @@ resource "boundary_user" "lambda" {
     boundary_account_password.lambda.id
   ]
 }
+
+# OIDC accounts
+resource "boundary_account_oidc" "oncall" {
+  name           = "john-call"
+  description    = "OIDC account for John Call"
+  auth_method_id = boundary_auth_method_oidc.provider.id
+  issuer         = boundary_auth_method_oidc.provider.issuer
+  subject        = azuread_user.oncall.object_id
+}
